249c0f9a0ac03b2bc32ab130b4d56b5c1492f80b
Nikolay <Nikolay.Solodukhin@gmail.com>
2016-02-22 18:20:28 +0300

    Почти финальная версия.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
- var link = document.querySelector(".feedback .btn")
- var popup = document.querySelector(".feedback-form")
- link.addEventListener("click", function (event) {
- event.preventDefault();
- popup.classList.add("feedback-form-show");
- });
-
+   var link = document.querySelector(".feedback .btn");
+   var popup = document.querySelector(".feedback-form");
+   link.addEventListener("click", function (event) {
+     event.preventDefault();
+     popup.classList.add("feedback-form-show");
+   });
+   var close = document.querySelector(".feedback-form .close");
+   close.addEventListener("click", function (event) {
+     event.preventDefault();
+     popup.classList.remove("feedback-form-show");
+   });
