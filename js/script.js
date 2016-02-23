bdf890fa74618ea629bcd3195452105d2530e9a1
Nikolay <Nikolay.Solodukhin@gmail.com>
2016-02-24 01:07:51 +0300

    Правки после проверки

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
    var link = document.querySelector(".feedback .btn");
    var popup = document.querySelector(".feedback-form");
+   var close = popup.querySelector(".feedback-form .close");
+   var form = popup.querySelector("form");
+   var firstname = popup.querySelector("[name=text]");
+   var firstemail = popup.querySelector("[name=email]");
    link.addEventListener("click", function (event) {
      event.preventDefault();
      popup.classList.add("feedback-form-show");
+     name.focus();
    });
-   var close = document.querySelector(".feedback-form .close");
    close.addEventListener("click", function (event) {
      event.preventDefault();
      popup.classList.remove("feedback-form-show");
+     popup.classList.remove("modal-error");
+   });
+   form.addEventListener("submit", function (event) {
+     if (!firstname.value || !firstemail1.value) {
+       event.preventDefault();
+       popup.classList.add("modal-error");
+     }
    });
