2c53217c814c3346def2f6f795f7ff2f5fb00e6b
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2016-08-12 23:45:20 +0300

    Добавил карту через API

--- js/script.js
+++ js/script.js
@@ -20,3 +20,20 @@
        popup.classList.add("modal-error");
      }
    });
+
+   function initialize() {
+     var mapOptions = {
+       zoom: 16,
+       center: new google.maps.LatLng(59.9386236, 30.3230243)
+     }
+     var map = new google.maps.Map(document.getElementById('map-canvas'),
+       mapOptions);
+     var image = "../img/map-pin.svg";
+     var myLatLng = new google.maps.LatLng(59.9386236, 30.3230243);
+     var beachMarker = new google.maps.Marker({
+       position: myLatLng,
+       map: map,
+       icon: image
+     });
+   }
+   google.maps.event.addDomListener(window, 'load', initialize);
